689b9a8dcdefc3de6f073d4517fda1c45a7b62ef
Luke Hoban <lukehoban@gmail.com>
2015-09-19 14:54:46 -0700

    Continue works

--- src/debugAdapter/openDebugMock.ts
+++ src/debugAdapter/openDebugMock.ts
@@ -9,6 +9,89 @@ import {basename, dirname} from 'path';
 import {spawn, ChildProcess} from 'child_process';
 import {Client, RPCConnection} from 'json-rpc2';
 
+interface DebuggerState {
+	exited: boolean;
+	exitStatus: number;
+	breakPoint: {
+		addr: number;
+		continue: boolean;
+		file: string;
+		functionName: string;
+		goroutine: boolean;
+		id: number;
+		line: number;
+		stacktrace: number;
+	}
+	breakPointInfo: {};
+	currentThread: {
+		file: string;
+		id: number;
+		line: number;
+		pc: number;
+	};
+	currentGoroutine: {
+		file: string;
+		function: {
+			args: {};
+			goType: number;
+			locals: {};
+			name: string;
+			type: number;
+			value: number;
+		};
+		id: number;
+		line: number;
+		pc: number;
+	};
+}
+
+interface DebuggerCommand {
+	name: string;
+}
+
+class Delve {
+	debugProcess: ChildProcess;
+	connection: Promise<RPCConnection>;
+	
+	constructor(program: string) {
+		this.connection = new Promise((resolve, reject) => {
+			var serverRunning = false;
+			this.debugProcess = spawn('/Users/lukeh/dd/go/bin/dlv', ['debug',  '--headless=true', '--listen=127.0.0.1:2345', '--log', program], { cwd: dirname(program) });
+			
+			function connectClient() {
+				var client = Client.$create(2345, '127.0.0.1');
+				client.connectSocket((err, conn) => {
+					if (err) return reject(err);
+					resolve(conn);			
+				});
+			}
+			
+			this.debugProcess.stderr.on('data', chunk => {
+				var str = chunk.toString();
+				console.log(str);
+				if(!serverRunning) {
+					serverRunning = true;
+					connectClient();
+				}
+			});
+			this.debugProcess.stdout.on('data', function(chunk) {
+				var str = chunk.toString();
+				console.log(str);
+			});
+			this.debugProcess.on('close', function(code) {
+				throw new Error(code);
+			});
+		});
+	}
+	
+	call<T>(command: string, args: any[], callback: (err: Error, results: T) => void) {
+		this.connection.then(conn => {
+			conn.call('RPCServer.' + command, args, callback);
+		}, err => {
+			callback(err, null);
+		});
+	}
+}
 
 class MockDebugSession extends DebugSession {
 
@@ -17,8 +100,10 @@ class MockDebugSession extends DebugSession {
 	private _sourceLines: string[];
 	private _breakPoints: any;
 	private _variableHandles: Handles<string>;
+	
+	private debugState: DebuggerState;
 	private debugProcess: ChildProcess;
-	private delve: RPCConnection;
+	private delve: Delve;
 
 	public constructor(debuggerLinesStartAt1: boolean, isServer: boolean = false) {
 		super(debuggerLinesStartAt1, isServer);
@@ -40,40 +125,18 @@ class MockDebugSession extends DebugSession {
 		this._sourceFile = args.program;
 		this._sourceLines = readFileSync(this._sourceFile).toString().split('\n');
 		
-		var serverRunning = false;
+		this.delve = new Delve(args.program);
 		
-		this.debugProcess = spawn('/Users/lukeh/dd/go/bin/dlv', ['debug',  '--headless=true', '--listen=127.0.0.1:2345', '--log', args.program], { cwd: dirname(args.program) });
-		this.debugProcess.stderr.on('data', chunk => {
-			var str = chunk.toString();
-			console.log(str);
-			if(!serverRunning) {
-				// Call add function on the server
-				var client = Client.$create(2345, '127.0.0.1');
-				client.connectSocket((err, conn) => {
-					if (err) return console.log("ERROR: " + err);
-					this.delve = conn;
-					this.delve.call('RPCServer.ProcessPid', [], (err, result) =>{
-						if(err) return console.log("ERROR: "+ err);
-						console.log("RESULT: " + result);
-						
-						if (args.stopOnEntry) {
-							this._currentLine = 0;
-							this.sendResponse(response);
-							this.sendEvent(new StoppedEvent("entry", 4711));
-						} else {
-							this.continueRequest(response);
-						}
-					});			
-				});
-				serverRunning = true;
-			}
-		})
-		this.debugProcess.stdout.on('data', function(chunk) {
-			var str = chunk.toString();
-			console.log(str);
-		})
-		this.debugProcess.on('close', function(code) {
-			throw new Error(code);
+		this.delve.call('ProcessPid', [], (err, result) =>{
+			if(err) return console.log("ERROR: "+ err);
+			console.log("RESULT: " + result);
+			// if (args.stopOnEntry) {
+			// 	this._currentLine = 0;
+				this.sendResponse(response);
+				this.sendEvent(new StoppedEvent("entry", 4711));
+			// } else {
+			// 	this.continueRequest(response);
+			// }
 		});
 	}
 
@@ -82,7 +145,7 @@ class MockDebugSession extends DebugSession {
 		for(var i = 0; i < args.lines.length; i++) {
 			var line = args.lines[i];
 			var file = args.source.path;
-			this.delve.call('RPCServer.CreateBreakpoint', [{file, line}], (err, result) => { 
+			this.delve.call('CreateBreakpoint', [{file, line}], (err, result) => { 
 				if(result) {
 					breakpoints.push({
 						verified: true,
@@ -114,9 +177,16 @@ class MockDebugSession extends DebugSession {
 	protected stackTraceRequest(response: OpenDebugProtocol.StackTraceResponse, args: OpenDebugProtocol.StackTraceArguments): void {
 
 		var frames = new Array<StackFrame>();
-		for (var i= 0; i < 3; i++) {
-			frames.push(new StackFrame(i, "frame " + i, new Source(basename(this._sourceFile), this.convertDebuggerPathToClient(this._sourceFile)), this.convertDebuggerLineToClient(this._currentLine), 0));
-		}
+		
+		frames.push(new StackFrame(
+			this.debugState.breakPoint.stacktrace, 
+			this.debugState.breakPoint.functionName, 
+			new Source(basename(this._sourceFile), 
+			this.convertDebuggerPathToClient(this._sourceFile)), 
+			this.convertDebuggerLineToClient(this._currentLine), 
+			0)
+		);
+		
 		response.body = {
 			stackFrames: frames
 		};
@@ -176,26 +246,18 @@ class MockDebugSession extends DebugSession {
 	}
 
 	protected continueRequest(response: OpenDebugProtocol.ContinueResponse): void {
-		var lines = this._breakPoints[this._sourceFile];
-		for (var ln = this._currentLine+1; ln < this._sourceLines.length; ln++) {
-			// is breakpoint on this line?
-			if (lines && lines.indexOf(ln) >= 0) {
-				this._currentLine = ln;
-				this.sendResponse(response);
-				this.sendEvent(new StoppedEvent("step", 4711));
-				return;
+		this.delve.call<DebuggerState>('Command', [{ name: 'continue' }], (err, result) => {
+			console.log(result);
+			if(result.exited) {
+				this.sendEvent(new TerminatedEvent());	
+			} else {
+				this._sourceFile = result.breakPoint.file;
+				this._currentLine = result.breakPoint.line;
+				this.debugState = result;
+				this.sendEvent(new StoppedEvent("breakpoint", 4711));
 			}
-			// if word 'exception' found in source -> throw exception
-			if (this._sourceLines[ln].indexOf("exception") >= 0) {
-				this._currentLine = ln;
-				this.sendResponse(response);
-				this.sendEvent(new StoppedEvent("exception", 4711));
-				return;
-			}
-		}
+		});
 		this.sendResponse(response);
-		// no more lines: run to end
-		this.sendEvent(new TerminatedEvent());
 	}
 
 	protected nextRequest(response: OpenDebugProtocol.NextResponse): void {
