fe7ec9eef99ce3599227d1e809cf1819bb87b245
Luke Hoban <lukehoban@gmail.com>
2015-11-27 09:52:03 -0800

    Semicolons :-)

--- src/goFormat.ts
+++ src/goFormat.ts
@@ -46,38 +46,38 @@ export class GoDocumentFormattingEditProvider implements vscode.DocumentFormatti
 					var text = stdout.toString();
 					var d = new dmp.diff_match_patch();
 
-					var diffs = d.diff_main(document.getText(), text)
-					var line = 0
-					var character = 0
-					var edits = new Array<vscode.TextEdit>()
+					var diffs = d.diff_main(document.getText(), text);
+					var line = 0;
+					var character = 0;
+					var edits: vscode.TextEdit[] = [];
 					for (var i = 0; i < diffs.length; i++) {
-						var start = new vscode.Position(line, character)
+						var start = new vscode.Position(line, character);
 
 						// Compute the line/character after the diff is applied.
 						for (var curr = 0; curr < diffs[i][1].length; curr++) {
 							if (diffs[i][1][curr] != '\n') {
-								character++
+								character++;
 							} else {
-								character = 0
-								line++
+								character = 0;
+								line++;
 							}
 						}
 						switch (diffs[i][0]) {
 							case dmp.DIFF_DELETE:
-								edits.push(vscode.TextEdit.delete(new vscode.Range(start, new vscode.Position(line, character))))
-								break
+								edits.push(vscode.TextEdit.delete(new vscode.Range(start, new vscode.Position(line, character))));
+								break;
 
 							case dmp.DIFF_INSERT:
 								// The edits are all relative to the original state of the document,
 								// so inserts should reset the current line/character position to
 								// the start.
-								line = start.line
-								character = start.character
-								edits.push(vscode.TextEdit.insert(start, diffs[i][1]))
-								break
-								
+								line = start.line;
+								character = start.character;
+								edits.push(vscode.TextEdit.insert(start, diffs[i][1]));
+								break;
+
 							case dmp.DIFF_EQUAL:
-								break
+								break;
 						}
 					}
 
