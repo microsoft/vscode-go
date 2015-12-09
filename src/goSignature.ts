05e86f9d04584a8307a06573aa04c6ab160df697
Luke Hoban <lukehoban@gmail.com>
2015-12-09 14:43:21 -0800

    Cleanup

--- src/goSignature.ts
+++ src/goSignature.ts
@@ -8,33 +8,32 @@
 import cp = require('child_process');
 import path = require('path');
 import { getBinPath } from './goPath'
-import { languages, window, commands, SignatureHelpProvider, SignatureHelp, SignatureInformation, ParameterInformation, TextDocument, Position, CancellationToken } from 'vscode';
+import { languages, window, commands, SignatureHelpProvider, SignatureHelp, SignatureInformation, ParameterInformation, TextDocument, Position, Range, CancellationToken } from 'vscode';
 import { definitionLocation } from "./goDeclaration"
 
 export class GoSignatureHelpProvider implements SignatureHelpProvider {
-	public provideSignatureHelp(document: TextDocument, position: Position, token: CancellationToken): Promise<SignatureHelp> {			let parenthesesPosition = this.lastParentheses(document, position);
-		if(parenthesesPosition == null) {
+
+	public provideSignatureHelp(document: TextDocument, position: Position, token: CancellationToken): Promise<SignatureHelp> {
+		let parenthesesPosition = this.lastParentheses(document, position);
+		if (parenthesesPosition == null) {
 			return null;
 		}
-
-		let callerPos = this.previousTokenPosition(document, parenthesesPosition);				
-
+		let callerPos = this.previousTokenPosition(document, parenthesesPosition);
 		return definitionLocation(document, callerPos).then(res => {
-			var result = new SignatureHelp();
-			let signatureBegin = res.lines[1].indexOf("(") // func Printf(a ...interface{}) => (a ...interface{})
-			var si = new SignatureInformation(res.lines[1].substring(signatureBegin), "");
-			
+			let result = new SignatureHelp();
+			let text = res.lines[1];
+			let nameEnd = text.indexOf(" ");
+			let sigStart = nameEnd + 5;
+			let si = new SignatureInformation(text.substring(0, nameEnd) + text.substring(sigStart),"");
 			result.signatures = [si];
 			result.activeSignature = 0;
 			result.activeParameter = 0;
 			return result;
-		}).catch(err => {
-			console.log(err);
 		});
 	}
-	
-	public previousTokenPosition(document: TextDocument, position: Position): Position {
-		while(position.character > 0) {
+
+	private previousTokenPosition(document: TextDocument, position: Position): Position {
+		while (position.character > 0) {
 			var word = document.getWordRangeAtPosition(position)
 			if (word) {
 				return word.start;
@@ -44,14 +43,14 @@ export class GoSignatureHelpProvider implements SignatureHelpProvider {
 		return null;
 	}
 
-	public lastParentheses(document: TextDocument, position: Position): Position {
+	private lastParentheses(document: TextDocument, position: Position): Position {
 		// TODO: handle double '(('
-		var currentLine = document.lineAt(position.line).text.substring(0,position.character);
+		var currentLine = document.lineAt(position.line).text.substring(0, position.character);
 		var lastIndex = currentLine.lastIndexOf("(");
-		
-		if(lastIndex < 0)
+
+		if (lastIndex < 0)
 			return null;
-		
+
 		return new Position(position.line, lastIndex);
 	}
 
