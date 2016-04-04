70809b6fd2ca2f9ea0250561b8cd27d7c9519b59
Luke Hoban <lukehoban@gmail.com>
2016-04-04 16:21:53 -0700

    Allow rename on last char of identifier.
    Fixes #268.

--- src/goRename.ts
+++ src/goRename.ts
@@ -22,7 +22,9 @@ export class GoRenameProvider implements vscode.RenameProvider {
 	private doRename(document: vscode.TextDocument, position: vscode.Position, newName: string, token: vscode.CancellationToken): Thenable<vscode.WorkspaceEdit> {
 		return new Promise((resolve, reject) => {
 			let filename = this.canonicalizeForWindows(document.fileName);
-			let offset = byteOffsetAt(document, position);
+			let range = document.getWordRangeAtPosition(position);
+			let pos = range ? range.start : position;
+			let offset = byteOffsetAt(document, pos);
 
 			let gorename = getBinPath('gorename');
 
@@ -32,7 +34,7 @@ export class GoRenameProvider implements vscode.RenameProvider {
 						vscode.window.showInformationMessage('The "gorename" command is not available.  Use "go get golang.org/x/tools/cmd/gorename" to install.');
 						return Promise.resolve<vscode.WorkspaceEdit>(null);
 					}
-					if (err) return reject('Cannot rename due to errors: ' + err);
+					if (err) return reject('Cannot rename due to errors: ' + stderr);
 					// TODO: 'gorename' makes the edits in the files out of proc.
 					// Would be better if we could get the list of edits.
 					return Promise.resolve<vscode.WorkspaceEdit>(null);
