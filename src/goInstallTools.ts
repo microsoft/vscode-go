875ecd4d08c1f9a8fe81c2726923ddd9023cef03
Ramya Achutha Rao <ramyar@microsoft.com>
2016-07-07 23:49:29 -0700

    set GO15VENDOREXPERIMENT=1 to support godoctor when using Go v1.5

--- src/goInstallTools.ts
+++ src/goInstallTools.ts
@@ -59,7 +59,7 @@ export function setupGoPathAndOfferToInstallTools() {
 		});
 		return;
 	}
-
+	
 	let keys = Object.keys(tools);
 	Promise.all(keys.map(tool => new Promise<string>((resolve, reject) => {
 		let toolPath = getBinPath(tool);
@@ -77,9 +77,9 @@ export function setupGoPathAndOfferToInstallTools() {
 		}
 	});
 
-	function promptForInstall(missing: string[]) {
+	function promptForInstall(missing: string[]) {		
 		// set GO15VENDOREXPERIMENT=1 to support godoctor when using Go v1.5
-		cp.exec('set GO15VENDOREXPERIMENT=1', { env: process.env }, (err, stdout, stderr) => {});
+		process.env['GO15VENDOREXPERIMENT'] = 1;
 
 		let item = {
 			title: 'Install',
