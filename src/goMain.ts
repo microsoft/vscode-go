b0467bdb08b87558a4f25aba9880e95c57b64b39
Martin Aeschlimann <martinae@microsoft.com>
2016-07-25 21:31:53 +0200

    Move away from deprecated language configuration settings

--- src/goMain.ts
+++ src/goMain.ts
@@ -86,35 +86,7 @@ export function activate(ctx: vscode.ExtensionContext): void {
 			// ^.*\{[^}'']*$
 			increaseIndentPattern: /^.*\{[^}'']*$/
 		},
-		wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\#\%\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g,
-		comments: {
-			lineComment: '//',
-			blockComment: ['/*', '*/']
-		},
-		brackets: [
-			['{', '}'],
-			['[', ']'],
-			['(', ')'],
-		],
-
-		__electricCharacterSupport: {
-			brackets: [
-				{ tokenType: 'delimiter.curly.ts', open: '{', close: '}', isElectric: true },
-				{ tokenType: 'delimiter.square.ts', open: '[', close: ']', isElectric: true },
-				{ tokenType: 'delimiter.paren.ts', open: '(', close: ')', isElectric: true }
-			]
-		},
-
-		__characterPairSupport: {
-			autoClosingPairs: [
-				{ open: '{', close: '}' },
-				{ open: '[', close: ']' },
-				{ open: '(', close: ')' },
-				{ open: '`', close: '`', notIn: ['string'] },
-				{ open: '"', close: '"', notIn: ['string'] },
-				{ open: '\'', close: '\'', notIn: ['string', 'comment'] }
-			]
-		}
+		wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\#\%\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g
 	});
 
 	if (vscode.window.activeTextEditor) {
