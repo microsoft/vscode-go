244fa1cc5a322630796301940f74a465bd88d097
Ramya Achutha Rao <ramyar@microsoft.com>
2016-09-15 18:50:18 -0700

    Rework the promises in the unit tests

--- test/go.test.ts
+++ test/go.test.ts
@@ -164,14 +164,13 @@ encountered.
 		}).then(() => done(), done);
 	});
 
-
 	test('Test diffUtils.getEditsFromUnifiedDiffStr', (done) => {
 		let file1path = path.join(fixtureSourcePath, 'diffTestData', 'file1.go');
 		let file2path = path.join(fixtureSourcePath, 'diffTestData', 'file2.go');
 		let file1uri = vscode.Uri.file(file1path);
 		let file2contents = fs.readFileSync(file2path, 'utf8');
 
-		return new Promise((resolve, reject) => {
+		let diffPromise = new Promise((resolve, reject) => {
 
 			cp.exec(`diff -u ${file1path} ${file2path}`, (err, stdout, stderr) => {
 				let filePatches = getEditsFromUnifiedDiffStr(stdout);
@@ -190,24 +189,24 @@ encountered.
 					assert.fail(null, null, 'Failed to parse edits from the diff output');
 					return reject();
 				}
+				resolve(filePatches);
+			});
+		});
 
-				vscode.workspace.openTextDocument(file1uri).then((textDocument) => {
-					vscode.window.showTextDocument(textDocument).then(editor => {
-						editor.edit((editBuilder) => {
-							filePatches[0].edits.forEach(edit => {
-								edit.applyUsingTextEditorEdit(editBuilder);
-							});
-						}).then(() => {
-							assert.equal(editor.document.getText(), file2contents);
-							vscode.commands.executeCommand('workbench.action.files.revert').then(() => {
-								resolve();
-							});
+		diffPromise.then((filePatches) => {
+			return vscode.workspace.openTextDocument(file1uri).then((textDocument) => {
+				return vscode.window.showTextDocument(textDocument).then(editor => {
+					return editor.edit((editBuilder) => {
+						filePatches[0].edits.forEach(edit => {
+							edit.applyUsingTextEditorEdit(editBuilder);
 						});
+					}).then(() => {
+						assert.equal(editor.document.getText(), file2contents);
+						return vscode.commands.executeCommand('workbench.action.files.revert');
 					});
 				});
 			});
 		}).then(() => done(), done);
-
 	});
 
 	test('Test diffUtils.getEdits', (done) => {
