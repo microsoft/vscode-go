8043a5cfdaae5d034c1910519ad8525bf5a92992
Ramya Achutha Rao <ramyar@microsoft.com>
2017-02-06 11:32:51 -0800

    Switch the condition

--- src/goPath.ts
+++ src/goPath.ts
@@ -105,5 +105,5 @@ export function resolvePath(inputPath: string, workspaceRoot?: string): string {
 	if (workspaceRoot) {
 		inputPath = inputPath.replace(/\${workspaceRoot}/g, workspaceRoot);
 	}
-	return !inputPath.startsWith('~') ? inputPath : path.join(os.homedir(), inputPath.substr(1));
+	return inputPath.startsWith('~') ? path.join(os.homedir(), inputPath.substr(1)) : inputPath;
 }
