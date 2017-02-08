a17f7a1adc14a59c4de0b951948650e852e07197
Richard Musiol <mail@richard-musiol.de>
2017-02-08 14:00:13 -0800

    fix offsets for files with multibyte characters
    When using multibyte characters like 🚨 in comments, actions like hover and go-to-def were using wrong offsets after that character.

--- src/util.ts
+++ src/util.ts
@@ -53,12 +53,7 @@ let telemtryReporter: TelemetryReporter;
 export function byteOffsetAt(document: vscode.TextDocument, position: vscode.Position): number {
 	let offset = document.offsetAt(position);
 	let text = document.getText();
-	let byteOffset = 0;
-	for (let i = 0; i < offset; i++) {
-		let clen = Buffer.byteLength(text[i]);
-		byteOffset += clen;
-	}
-	return byteOffset;
+	return Buffer.byteLength(text.substr(0, offset));
 }
 
 export interface Prelude {
