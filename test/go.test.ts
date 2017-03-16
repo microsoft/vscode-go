829791f582d84d63aa655eeb56bcfc504e42e5eb
Ramya Rao <ramya.rao.a@outlook.com>
2017-03-16 11:52:49 -0700

    Fix failing metalinner tests (#867)

--- test/go.test.ts
+++ test/go.test.ts
@@ -421,8 +421,8 @@ It returns the number of bytes written and any write error encountered.
 				{ line: 11, severity: 'warning', msg: 'undeclared name: prin (staticcheck)' },
 				{ line: 11, severity: 'warning', msg: 'undeclared name: prin (unconvert)' },
 				{ line: 11, severity: 'error', msg: 'undefined: prin' },
-				{ line: 11, severity: 'warning', msg: 'unused global variable undeclared name: prin (varcheck)' },
 				{ line: 11, severity: 'warning', msg: 'unused struct field undeclared name: prin (structcheck)' },
+				{ line: 11, severity: 'warning', msg: 'unused variable or constant undeclared name: prin (varcheck)' },
 			];
 			return check(path.join(fixturePath, 'errorsTest', 'errors.go'), config).then(diagnostics => {
 				let sortedDiagnostics = diagnostics.sort((a, b) => {
