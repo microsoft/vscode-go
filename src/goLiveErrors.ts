aaf028abf5392cbd376214248e051479e96d6a20
Tyler Bunnell <tylerbunnell@gmail.com>
2017-04-02 16:36:44 -0600

    Use separate diagnosticCollections for warn/err

--- src/goLiveErrors.ts
+++ src/goLiveErrors.ts
@@ -5,7 +5,7 @@ import { byteOffsetAt, getBinPath } from './util';
 import cp = require('child_process');
 import path = require('path');
 import { promptForMissingTool } from './goInstallTools';
-import { diagnosticCollection } from './goMain';
+import { errorDiagnosticCollection } from './goMain';
 
 // Interface for settings configuration for adding and removing tags
 interface GoLiveErrorsConfig {
@@ -62,32 +62,16 @@ function processFile(e: vscode.TextDocumentChangeEvent) {
 			return;
 		}
 
-		// we want to take the error path here because the command we are calling
-		// returns a non-zero exit status if the checks fail
-		let newDiagnostics = [];
-		if (!newDiagnostics) {
-			newDiagnostics = [];
-		}
-
-		// grab a copy of the existing diagnostics that are being reported for the
-		// current file
-		let oldDiagnostics = diagnosticCollection.get(uri);
-
-		// delete the existing diagnostics for the current file
-		//
-		// error-level diagnostics will be reported by this process, so we want to
-		// clear out the existing errors to avoid getting duplicates
-		diagnosticCollection.delete(uri);
-
-		// we want to keep all non-error level diagnostics that were previously
-		// reported, so add them back in
-		oldDiagnostics.forEach((value) => {
-			if (value.severity !== vscode.DiagnosticSeverity.Error) {
-				newDiagnostics.push(value);
-			}
-		});
+		errorDiagnosticCollection.delete(uri);
 
 		if (err) {
+			// we want to take the error path here because the command we are calling
+			// returns a non-zero exit status if the checks fail
+			let diagnostics = [];
+			if (!diagnostics) {
+				diagnostics = [];
+			}
+
 			stderr.split('\n').forEach(error => {
 				if (error === null || error.length === 0) {
 					return;
@@ -97,10 +81,11 @@ function processFile(e: vscode.TextDocumentChangeEvent) {
 
 				let range = new vscode.Range(+line - 1, +column, +line - 1, +column);
 				let diagnostic = new vscode.Diagnostic(range, message, vscode.DiagnosticSeverity.Error);
-				newDiagnostics.push(diagnostic);
+				diagnostics.push(diagnostic);
 			});
+
+			errorDiagnosticCollection.set(uri, diagnostics);
 		}
-		diagnosticCollection.set(uri, newDiagnostics);
 	});
 	p.stdin.end(fileContents);
 }
--- src/goMain.ts
+++ src/goMain.ts
@@ -34,7 +34,8 @@ import { clearCacheForTools } from './goPath';
 import { addTags, removeTags } from './goModifytags';
 import { parseLiveFile } from './goLiveErrors';
 
-export let diagnosticCollection: vscode.DiagnosticCollection;
+export let errorDiagnosticCollection: vscode.DiagnosticCollection;
+let warningDiagnosticCollection: vscode.DiagnosticCollection;
 
 export function activate(ctx: vscode.ExtensionContext): void {
 	let useLangServer = vscode.workspace.getConfiguration('go')['useLanguageServer'];
@@ -82,8 +83,10 @@ export function activate(ctx: vscode.ExtensionContext): void {
 	ctx.subscriptions.push(vscode.languages.registerRenameProvider(GO_MODE, new GoRenameProvider()));
 	ctx.subscriptions.push(vscode.languages.registerCodeActionsProvider(GO_MODE, new GoCodeActionProvider()));
 
-	diagnosticCollection = vscode.languages.createDiagnosticCollection('go');
-	ctx.subscriptions.push(diagnosticCollection);
+	errorDiagnosticCollection = vscode.languages.createDiagnosticCollection('go-error');
+	ctx.subscriptions.push(errorDiagnosticCollection);
+	warningDiagnosticCollection = vscode.languages.createDiagnosticCollection('go-warning');
+	ctx.subscriptions.push(warningDiagnosticCollection);
 	vscode.workspace.onDidChangeTextDocument(removeCodeCoverage, null, ctx.subscriptions);
 	vscode.workspace.onDidChangeTextDocument(removeTestStatus, null, ctx.subscriptions);
 	vscode.window.onDidChangeActiveTextEditor(showHideStatus, null, ctx.subscriptions);
@@ -233,9 +236,9 @@ function runBuilds(document: vscode.TextDocument, goConfig: vscode.WorkspaceConf
 
 	let uri = document.uri;
 	check(uri.fsPath, goConfig).then(errors => {
-		diagnosticCollection.clear();
+		errorDiagnosticCollection.clear();
 
-		let diagnosticMap: Map<string, vscode.Diagnostic[]> = new Map();
+		let diagnosticMap: Map<string, Map<vscode.DiagnosticSeverity, vscode.Diagnostic[]>> = new Map();
 
 		errors.forEach(error => {
 			let canonicalFile = vscode.Uri.file(error.file).toString();
@@ -249,16 +252,21 @@ function runBuilds(document: vscode.TextDocument, goConfig: vscode.WorkspaceConf
 				endColumn = text.length - trailing.length;
 			}
 			let range = new vscode.Range(error.line - 1, startColumn, error.line - 1, endColumn);
-			let diagnostic = new vscode.Diagnostic(range, error.msg, mapSeverityToVSCodeSeverity(error.severity));
+			let severity = mapSeverityToVSCodeSeverity(error.severity);
+			let diagnostic = new vscode.Diagnostic(range, error.msg, severity);
 			let diagnostics = diagnosticMap.get(canonicalFile);
 			if (!diagnostics) {
-				diagnostics = [];
+				diagnostics = new Map<vscode.DiagnosticSeverity, vscode.Diagnostic[]>();
 			}
-			diagnostics.push(diagnostic);
+			if (!diagnostics[severity]) {
+				diagnostics[severity] = [];
+			}
+			diagnostics[severity].push(diagnostic);
 			diagnosticMap.set(canonicalFile, diagnostics);
 		});
-		diagnosticMap.forEach((diags, file) => {
-			diagnosticCollection.set(vscode.Uri.parse(file), diags);
+		diagnosticMap.forEach((diagMap, file) => {
+			errorDiagnosticCollection.set(vscode.Uri.parse(file), diagMap[vscode.DiagnosticSeverity.Error]);
+			warningDiagnosticCollection.set(vscode.Uri.parse(file), diagMap[vscode.DiagnosticSeverity.Warning]);
 		});
 	}).catch(err => {
 		vscode.window.showInformationMessage('Error: ' + err);
