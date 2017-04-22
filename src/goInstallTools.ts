4e01cc5b0d66575ea7860ff9604370043588a2ee
Jamie Stackhouse <contin673@gmail.com>
2017-04-22 12:12:21 -0300

    Add goimpl to tools list.

--- src/goInstallTools.ts
+++ src/goInstallTools.ts
@@ -27,7 +27,8 @@ function getTools(goVersion: SemVersion): { [key: string]: string } {
 		'go-symbols': 'github.com/acroca/go-symbols',
 		'guru': 'golang.org/x/tools/cmd/guru',
 		'gorename': 'golang.org/x/tools/cmd/gorename',
-		'gomodifytags': 'github.com/fatih/gomodifytags'
+		'gomodifytags': 'github.com/fatih/gomodifytags',
+		'impl': 'github.com/josharian/impl'
 	};
 	if (goLiveErrorsEnabled()) {
 		tools['gotype-live'] = 'github.com/tylerb/gotype-live';
