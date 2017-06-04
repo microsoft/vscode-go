41676551cf2f6e8bf5e7c51660a7eb9c73754e6f
Jamie Stackhouse <itsjamie@users.noreply.github.com>
2017-06-04 02:47:19 -0300

    Goimpl implementation (#939)
    * Add goimpl to tools list.
    
    * Support github.com/josharian/impl. Fixes #259.
    
    Does some basic input validation to ensure the required information is
    passed.
    
    The regex included makes the receiver name optional, the intention there
    is that if the receiver name isn't included, that the type the cursor is
    in should be used as the receiver for the interface.
    
    However, I think that the first step of just having the ability to use
    goimpl was a important first step.
    
    * Missing semicolon lint fix.
    
    * Add `go.impl.cursor` to package.json

--- src/goMain.ts
+++ src/goMain.ts
@@ -32,6 +32,7 @@ import { clearCacheForTools } from './goPath';
 import { addTags, removeTags } from './goModifytags';
 import { parseLiveFile } from './goLiveErrors';
 import { GoCodeLensProvider } from './goCodelens';
+import { implCursor } from './goImpl';
 
 export let errorDiagnosticCollection: vscode.DiagnosticCollection;
 let warningDiagnosticCollection: vscode.DiagnosticCollection;
@@ -114,6 +115,10 @@ export function activate(ctx: vscode.ExtensionContext): void {
 
 	ctx.subscriptions.push(vscode.commands.registerCommand('go.remove.tags', (args) => {
 		removeTags(args);
+	}));
+
+	ctx.subscriptions.push(vscode.commands.registerCommand('go.impl.cursor', () => {
+		implCursor();
 	}));
 
 	ctx.subscriptions.push(vscode.commands.registerCommand('go.test.cursor', (args) => {
