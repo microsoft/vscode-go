12f92c907ca24647389b00be23013393fdb7f401
Nuruddin Ashr <uudashr@gmail.com>
2017-08-02 07:39:21 +0700

    Return the output immediately both for stdout and stderr (#1124)
    * Return the output immediately stdout and stderr
    
    * Fix lint error

--- src/util.ts
+++ src/util.ts
@@ -307,3 +307,42 @@ export function getExtensionCommands(): any[] {
 	let extensionCommands: any[] = vscode.extensions.getExtension(extensionId).packageJSON.contributes.commands.filter(x => x.command !== 'go.show.commands');
 	return extensionCommands;
 }
+
+export class LineBuffer {
+	private buf: string = '';
+	private lineListeners: { (line: string): void; }[] = [];
+	private lastListeners: { (last: string): void; }[] = [];
+
+	append(chunk: string) {
+		this.buf += chunk;
+		do {
+			const idx = this.buf.indexOf('\n');
+			if (idx === -1) {
+				break;
+			}
+
+			this.fireLine(this.buf.substring(0, idx));
+			this.buf = this.buf.substring(idx + 1);
+		} while (true);
+	}
+
+	done() {
+		this.fireDone(this.buf !== '' ? this.buf : null);
+	}
+
+	private fireLine(line: string) {
+		this.lineListeners.forEach(listener => listener(line));
+	}
+
+	private fireDone(last: string) {
+		this.lastListeners.forEach(listener => listener(last));
+	}
+
+	onLine(listener: (line: string) => void) {
+		this.lineListeners.push(listener);
+	}
+
+	onDone(listener: (last: string) => void) {
+		this.lastListeners.push(listener);
+	}
+}
