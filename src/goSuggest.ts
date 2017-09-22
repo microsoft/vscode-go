cf0c68a5e256987d8f9b77f6a373bc5d5174594d
Nuruddin Ashr <uudashr@gmail.com>
2017-09-22 17:59:06 +0700

    Always resolve

--- src/goSuggest.ts
+++ src/goSuggest.ts
@@ -241,8 +241,8 @@ export class GoCompletionItemProvider implements vscode.CompletionItemProvider {
 								packageItem.kind = vscode.CompletionItemKind.Snippet;
 								packageItem.insertText = 'package ' + pkgName + '\r\n\r\n';
 								suggestions.push(packageItem);
-								resolve(suggestions);
 							}
+							resolve(suggestions);
 						});
 					}
 
