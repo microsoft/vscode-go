b63ebb8ab901ad17c48ff04a0c2f416f8048f118
Ramya Achutha Rao <ramyar@microsoft.com>
2017-09-25 08:25:39 -0700

    Adding file schema back as per disucssions in Microsoft/vscode#34034

--- src/goMode.ts
+++ src/goMode.ts
@@ -7,4 +7,4 @@
 
 import vscode = require('vscode');
 
-export const GO_MODE: vscode.DocumentFilter = { language: 'go' };
+export const GO_MODE: vscode.DocumentFilter = { language: 'go', scheme: 'file' };
