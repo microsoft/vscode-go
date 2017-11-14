35fc3c2b79cc5fddae4612d92aad4033a5db41bb
Michael Hanley <me@hanleym.com>
2017-11-14 11:27:50 -0500

    Fixed lint errors.

--- src/goExpanderr.ts
+++ src/goExpanderr.ts
@@ -8,22 +8,22 @@ import { getBinPath, getToolsEnvVars } from './util';
 
 export function runExpanderr(): Thenable<string> {
 	return new Promise<string>((resolve, reject) => {
-		try{
+		try {
 			let editor = vscode.window.activeTextEditor;
 
-			if(!editor){
+			if (!editor) {
 				vscode.window.showInformationMessage('No editor selected.');
-				return reject('No editor selected.')
+				return reject('No editor selected.');
 			}
 
-			if(!editor.document.fileName.endsWith('.go')){
+			if (!editor.document.fileName.endsWith('.go')) {
 				vscode.window.showInformationMessage('File in the editor is not a Go file.');
-				return reject('File in the editor is not a Go file.')
+				return reject('File in the editor is not a Go file.');
 			}
 
-			if(editor.document.isDirty){
+			if (editor.document.isDirty) {
 				vscode.window.showInformationMessage('File has unsaved changes. Save and try again.');
-				return reject('File has unsaved changes. Save and try again.')
+				return reject('File has unsaved changes. Save and try again.');
 			}
 
 			let filename = editor.document.uri.fsPath;
@@ -33,10 +33,10 @@ export function runExpanderr(): Thenable<string> {
 			let goConfig = vscode.workspace.getConfiguration('go', editor.document.uri);
 			let env = getToolsEnvVars();
 
-			cp.execFile(binPath, [filename+':#'+offset], {env}, (err, stdout, stderr) => {
-				if(err && (<any>err).code === 'ENOENT'){
+			cp.execFile(binPath, [filename + ':#' + offset], {env}, (err, stdout, stderr) => {
+				if (err && (<any>err).code === 'ENOENT') {
 					promptForMissingTool('expanderr');
-					return reject("Missing tool: expanderr");
+					return reject('Missing tool: expanderr');
 				}
 
 				if (err) {
