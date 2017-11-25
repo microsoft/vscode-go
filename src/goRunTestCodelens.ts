9a04c692506d3b514346b9cebb11c7e47b34d2a4
Ramya Achutha Rao <ramyar@microsoft.com>
2017-11-24 22:57:14 -0800

    Check for codelens before concat Fixes #1367

--- src/goRunTestCodelens.ts
+++ src/goRunTestCodelens.ts
@@ -15,14 +15,14 @@ import { GoBaseCodeLensProvider } from './goBaseCodelens';
 
 export class GoRunTestCodeLensProvider extends GoBaseCodeLensProvider {
 	private readonly debugConfig: any = {
-				'name': 'Launch',
-				'type': 'go',
-				'request': 'launch',
-				'mode': 'test',
-				'env': {
-					'GOPATH': getCurrentGoPath() // Passing current GOPATH to Delve as it runs in another process
-				}
-			};
+		'name': 'Launch',
+		'type': 'go',
+		'request': 'launch',
+		'mode': 'test',
+		'env': {
+			'GOPATH': getCurrentGoPath() // Passing current GOPATH to Delve as it runs in another process
+		}
+	};
 
 	public provideCodeLenses(document: TextDocument, token: CancellationToken): CodeLens[] | Thenable<CodeLens[]> {
 		if (!this.enabled) {
@@ -38,30 +38,37 @@ export class GoRunTestCodeLensProvider extends GoBaseCodeLensProvider {
 		return Promise.all([
 			this.getCodeLensForPackage(document, token),
 			this.getCodeLensForFunctions(config, document, token)
-		]).then(res => {
-			return res[0].concat(res[1]);
+		]).then(([pkg, fns]) => {
+			const res = [];
+			if (pkg && Array.isArray(pkg)) {
+				res.concat(pkg);
+			}
+			if (fns && Array.isArray(fns)) {
+				res.concat(fns);
+			}
+			return res;
 		});
 	}
 
 	private getCodeLensForPackage(document: TextDocument, token: CancellationToken): Thenable<CodeLens[]> {
 		let documentSymbolProvider = new GoDocumentSymbolProvider();
 		return documentSymbolProvider.provideDocumentSymbols(document, token)
-				.then(symbols => symbols.find(sym => sym.kind === vscode.SymbolKind.Package && !!sym.name))
-				.then(pkg => {
-					if (pkg) {
-						const range = pkg.location.range;
-						return [
-							new CodeLens(range, {
-								title: 'run package tests',
-								command: 'go.test.package'
-							}),
-							new CodeLens(range, {
-								title: 'run file tests',
-								command: 'go.test.file'
-							})
-						];
-					}
-				});
+			.then(symbols => symbols.find(sym => sym.kind === vscode.SymbolKind.Package && !!sym.name))
+			.then(pkg => {
+				if (pkg) {
+					const range = pkg.location.range;
+					return [
+						new CodeLens(range, {
+							title: 'run package tests',
+							command: 'go.test.package'
+						}),
+						new CodeLens(range, {
+							title: 'run file tests',
+							command: 'go.test.file'
+						})
+					];
+				}
+			});
 	}
 
 	private getCodeLensForFunctions(vsConfig: vscode.WorkspaceConfiguration, document: TextDocument, token: CancellationToken): Thenable<CodeLens[]> {
@@ -72,12 +79,12 @@ export class GoRunTestCodeLensProvider extends GoBaseCodeLensProvider {
 				let runTestCmd: Command = {
 					title: 'run test',
 					command: 'go.test.cursor',
-					arguments: [ { functionName: func.name } ]
+					arguments: [{ functionName: func.name }]
 				};
 
 				const args = ['-test.run', func.name];
 				const program = path.dirname(document.fileName);
-				const env = Object.assign({ }, this.debugConfig.env, vsConfig['testEnvVars']);
+				const env = Object.assign({}, this.debugConfig.env, vsConfig['testEnvVars']);
 				const envFile = vsConfig['testEnvFile'];
 				let buildFlags = getTestFlags(vsConfig, null);
 				if (vsConfig['buildTags'] && buildFlags.indexOf('-tags') === -1) {
@@ -89,7 +96,7 @@ export class GoRunTestCodeLensProvider extends GoBaseCodeLensProvider {
 				let debugTestCmd: Command = {
 					title: 'debug test',
 					command: 'go.debug.startSession',
-					arguments: [ config ]
+					arguments: [config]
 				};
 
 				codelens.push(new CodeLens(func.location.range, runTestCmd));
@@ -102,7 +109,7 @@ export class GoRunTestCodeLensProvider extends GoBaseCodeLensProvider {
 				let runBenchmarkCmd: Command = {
 					title: 'run benchmark',
 					command: 'go.benchmark.cursor',
-					arguments: [ { functionName: func.name } ]
+					arguments: [{ functionName: func.name }]
 				};
 
 				codelens.push(new CodeLens(func.location.range, runBenchmarkCmd));
