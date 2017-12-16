c7fe79f068f104dfa6ee9d8fa479d54a3d66898a
Avihay Kain <2963806+grooveygr@users.noreply.github.com>
2017-12-17 00:56:02 +0200

    Complete method receivers of types in namespace #168 (#1368)
    * Complete method receivers of types in namespace #168
    
    * tuned suggestion naming
    
    * use filterText for better labeling

--- src/util.ts
+++ src/util.ts
@@ -45,6 +45,29 @@ export const goKeywords: string[] = [
 	'var'
 ];
 
+export const goBuiltinTypes: Set<string> = new Set<string>([
+	'bool',
+	'byte',
+	'complex128',
+	'complex64',
+	'error',
+	'float32',
+	'float64',
+	'int',
+	'int16',
+	'int32',
+	'int64',
+	'int8',
+	'rune',
+	'string',
+	'uint',
+	'uint16',
+	'uint32',
+	'uint64',
+	'uint8',
+	'uintptr'
+]);
+
 export interface SemVersion {
 	major: number;
 	minor: number;
