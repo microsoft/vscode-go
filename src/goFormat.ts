77f1fa2d57c3027f6d64e8474bf95b0ab6481aa4
Jackson Kearl <jkearl@mit.edu>
2018-08-13 18:52:08 -0700

    remove github merge tools nonsense

--- src/goFormat.ts
+++ src/goFormat.ts
@@ -34,7 +34,7 @@ export class GoDocumentFormattingEditProvider implements vscode.DocumentFormatti
 			formatFlags.push('-style=indent=' + options.tabSize);
 		}
 
-		return this.runFormatter(formatTool, formatFlags, document, token).then(edits => edits, err => {		
+		return this.runFormatter(formatTool, formatFlags, document, token).then(edits => edits, err => {
 			if (typeof err === 'string' && err.startsWith('flag provided but not defined: -srcdir')) {
 				promptForUpdatingTool(formatTool);
 				return Promise.resolve([]);
