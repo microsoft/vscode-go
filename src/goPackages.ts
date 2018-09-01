e4399879ba7918bfa6ff7f4dd552a2cede360037
Nuruddin Ashr <uudashr@gmail.com>
2018-09-01 23:20:42 +0700

    Autocomplete dependencies based on go.mod

--- src/goPackages.ts
+++ src/goPackages.ts
@@ -151,6 +151,31 @@ export function getAllPackages(workDir?: string): Promise<Map<string, string>> {
 	});
 }
 
+function getModulePackages(folderPath: string): Promise<Map<string, string>> {
+	let goRuntimePath = getBinPath('go');
+	return new Promise<Map<string, string>>((resolve) => {
+		cp.execFile(goRuntimePath, ['list', '-m', '-f', '{{.Path}}', 'all'], { cwd: folderPath, env: getToolsEnvVars() }, (error, stdout, stderr) => {
+			const pkgMap = new Map<string, string>();
+
+			if (error) {
+				// module might not supported or turned off
+				return resolve(pkgMap);
+			}
+
+			stdout.split('\n').forEach(line => {
+				line = line.trim();
+				if (line === '') {
+					return;
+				}
+
+				pkgMap.set(line, line.substring(line.lastIndexOf('/') + 1));
+			});
+			return resolve(pkgMap);
+		});
+	});
+}
+
+
 /**
  * Returns mapping of import path and package name for packages that can be imported
  * Possible to return empty if useCache options is used.
@@ -174,7 +199,7 @@ export function getImportablePackages(filePath: string, useCache: boolean = fals
 	}
 
 
-	return Promise.all([isVendorSupported(), getAllPackagesPromise]).then(([vendorSupported, pkgs]) => {
+	return Promise.all([isVendorSupported(), getAllPackagesPromise, getModulePackages(fileDirPath)]).then(([vendorSupported, pkgs, modPkgs]) => {
 		let pkgMap = new Map<string, string>();
 		if (!pkgs) {
 			return pkgMap;
@@ -209,6 +234,10 @@ export function getImportablePackages(filePath: string, useCache: boolean = fals
 			if (allowToImport) {
 				pkgMap.set(relativePkgPath, pkgName);
 			}
+		});
+
+		modPkgs.forEach((pkgName, pkgPath) => {
+			pkgMap.set(pkgPath, pkgName);
 		});
 		return pkgMap;
 	});
