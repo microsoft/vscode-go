78987148dcdaf5792ded5232c19ad72102730917
karthikraobr <karthik.0703@gmail.com>
2018-10-16 00:50:54 +0200

    Fixed an issue when the change was on multiple lines

--- src/goCover.ts
+++ src/goCover.ts
@@ -314,11 +314,10 @@ export function isPartOfComment(e: vscode.TextDocumentChangeEvent): boolean {
 	let result = false
 	e.contentChanges.every(function(change, index){
 		let line = e.document.lineAt(change.range.start)
-		let text = e.document.getText(line.rangeIncludingLineBreak)
+		let text = e.document.getText(line.range)
 		let idx = text.search('//')
 		let changeIdx = change.range.start.character
-		// Exit the loop as soon as one of the changes is not part of a comment.
-		if (idx != -1 && idx > changeIdx){
+		if (idx == -1 || idx > changeIdx || !change.range.isSingleLine || change.text.includes("\n") ){
 			result = false
 			return false;
 		}else{
@@ -326,14 +325,4 @@ export function isPartOfComment(e: vscode.TextDocumentChangeEvent): boolean {
 		}
 	})
 	return result
-}
-
-export function isPartOfMultiLineComment(lineNumber: number, doc: vscode.TextDocument): boolean{
-	do{
-		let text = doc.lineAt(lineNumber).text
-		if (text.search("*/")){
-			return true
-		}
-	}while(lineNumber<doc.lineCount)
-	return false
 }
