f267d85d2e85bf52f97b515acb5d4ee75818d9e4
Kerem Gocen <keremgocen@gmail.com>
2019-01-08 04:11:12 +0300

    fix formatting

--- src/goPath.ts
+++ src/goPath.ts
@@ -90,7 +90,7 @@ function fileExists(filePath: string): boolean {
 	let flag = true;
 	try {
 		fs.accessSync(filePath, fs.constants.F_OK | fs.constants.X_OK);
-	}	catch	(e)	{
+	} catch (e) {
 		flag = false;
 	}
 	return flag;
