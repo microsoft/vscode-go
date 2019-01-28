d5dfc5c2f3f2793124f3923e88d4d79d0371a39c
Vlad Barosan <vladdb@microsoft.com>
2019-01-28 10:57:34 -0800

    Fix for test suits and refactor

--- src/goDebugTest.ts
+++ src/goDebugTest.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import vscode = require('vscode');
-import { getTestFunctions } from './testUtils';
+import { getTestFunctions, extractInstanceTestName, findAllTestSuiteRuns, getTestFunctionDebugArgs } from './testUtils';
 
 /**
 * Debugs the unit test at the primary cursor. Output
@@ -10,7 +10,7 @@ import { getTestFunctions } from './testUtils';
 * @param goConfig Configuration for the Go extension.
 */
 export function debugTestAtCursor(goConfig: vscode.WorkspaceConfiguration, args: any) {
-	let editor = vscode.window.activeTextEditor;
+	const editor = vscode.window.activeTextEditor;
 	if (!editor) {
 		vscode.window.showInformationMessage('No editor is active.');
 		return;
@@ -20,46 +20,35 @@ export function debugTestAtCursor(goConfig: vscode.WorkspaceConfiguration, args:
 		return;
 	}
 
-	getTestFunctions(editor.document, null).then(testFunctions => {
-		let testFunctionName: string;
+	editor.document.save().then(() => {
+		return getTestFunctions(editor.document, null).then(testFunctions => {
+			// We use functionName if it was provided as argument
+			// Otherwise find any test function containing the cursor.
+			const testFunctionName = args && args.functionName
+				? args.functionName
+				: testFunctions.find(func => func.location.range.contains(editor.selection.start)).name;
 
-		// We use functionName if it was provided as argument
-		// Otherwise find any test function containing the cursor.
-		if (args && args.functionName) {
-			testFunctionName = args.functionName;
-		} else {
-			for (let func of testFunctions) {
-				let selection = editor.selection;
-				if (selection && func.location.range.contains(selection.start)) {
-					testFunctionName = func.name;
-					break;
-				}
+			if (!testFunctionName) {
+				vscode.window.showInformationMessage('No test function found at cursor.');
+				return;
 			}
-		}
 
-		if (!testFunctionName) {
-			vscode.window.showInformationMessage('No test function found at cursor.');
-			return;
-		}
+			const dbgArgs = getTestFunctionDebugArgs(editor.document, testFunctionName, testFunctions);
+			const workspaceFolder = vscode.workspace.getWorkspaceFolder(editor.document.uri);
+			const debugConfig: vscode.DebugConfiguration = {
+				name: 'Debug Test',
+				type: 'go',
+				request: 'launch',
+				mode: 'auto',
+				program: editor.document.fileName,
+				env: goConfig.get('testEnvVars', {}),
+				envFile: goConfig.get('testEnvFile'),
+				args: dbgArgs
+			};
 
-		const configArgs = ['-test.run', testFunctionName];
-		const env = goConfig['testEnvVars'] || {};
-		const envFile = goConfig['testEnvFile'];
-
-		let workspaceFolder = vscode.workspace.getWorkspaceFolder(editor.document.uri);
-		let debugConfig: vscode.DebugConfiguration = {
-			'name': 'Debug Test',
-			'type': 'go',
-			'request': 'launch',
-			'mode': 'auto',
-			'program': editor.document.fileName,
-			'env': env,
-			'envFile': envFile,
-			'args': configArgs
-		};
-
-		return vscode.debug.startDebugging(workspaceFolder, debugConfig);
-	}).then(null, err => {
-		console.log(err);
+			return vscode.debug.startDebugging(workspaceFolder, debugConfig);
+		}).then(null, err => {
+			console.log(err);
+		});
 	});
 }
