e20ad850c9702349c608daf10d784724fe361cad
SteelPhase <SteelPhase@users.noreply.github.com>
2019-05-22 10:51:02 -0400

    add initial attempt at restarting lang server

--- src/goMain.ts
+++ src/goMain.ts
@@ -287,6 +287,12 @@ export function activate(ctx: vscode.ExtensionContext): void {
 
 			});
 
+			ctx.subscriptions.push(vscode.commands.registerCommand('go.languageserver.restart', () => {
+				const thing = c.stop().then(() => {
+					c.start();
+				});
+			}));
+
 			ctx.subscriptions.push(c.start());
 		} else {
 			registerCompletionProvider(ctx);
