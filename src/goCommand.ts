afab983d65bbed461f3d38276e4a8eeeb6b7e27c
Rebecca Stambler <rstambler@golang.org>
2019-09-25 14:26:56 -0400

    begin working on utility functions for go commands

---
+++ src/goCommand.ts
@@ -0,0 +1,14 @@
+import { getBinPath } from "./util";
+
+export function goGet() {
+	const goRuntimePath = getBinPath('go');
+
+}
+
+export function goList() {
+	const goRuntimePath = getBinPath('go');
+}
+
+export function goEnv() {
+	const goRuntimePath = getBinPath('go');
+}
