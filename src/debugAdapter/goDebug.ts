4f70a8e4cebf34a1c96206ce6d403620305c20e0
Ramya Achutha Rao <ramyar@microsoft.com>
2019-10-23 22:35:57 -0700

    Use json object instead of maps

--- src/debugAdapter/goDebug.ts
+++ src/debugAdapter/goDebug.ts
@@ -232,7 +232,7 @@ interface LaunchRequestArguments extends DebugProtocol.LaunchRequestArguments {
 
 	showGlobalVariables?: boolean;
 	currentFile: string;
-	packagePathToGoModPathMap: Map<string, string>;
+	packagePathToGoModPathMap: {[key: string]: string};
 }
 
 interface AttachRequestArguments extends DebugProtocol.AttachRequestArguments {
@@ -435,7 +435,7 @@ class Delve {
 				dlvArgs.push(mode || 'debug');
 				if (mode === 'exec') {
 					dlvArgs.push(program);
-				} else if (currentGOWorkspace && !launchArgs.packagePathToGoModPathMap.get(dirname)) {
+				} else if (currentGOWorkspace && !launchArgs.packagePathToGoModPathMap[dirname]) {
 					dlvArgs.push(dirname.substr(currentGOWorkspace.length + 1));
 				}
 				dlvArgs.push('--headless=true', `--listen=${launchArgs.host}:${launchArgs.port}`);
--- src/goModules.ts
+++ src/goModules.ts
@@ -38,12 +38,12 @@ export function isModSupported(fileuri: vscode.Uri): Promise<boolean> {
 	return getModFolderPath(fileuri).then(modPath => !!modPath);
 }
 
-export const packagePathToGoModPathMap = new Map<string, string>();
+export const packagePathToGoModPathMap: {[key: string]: string} = {};
 
 export async function getModFolderPath(fileuri: vscode.Uri): Promise<string> {
 	const pkgPath = path.dirname(fileuri.fsPath);
-	if (packagePathToGoModPathMap.has(pkgPath)) {
-		return packagePathToGoModPathMap.get(pkgPath);
+	if (packagePathToGoModPathMap[pkgPath]) {
+		return packagePathToGoModPathMap[pkgPath];
 	}
 
 	// We never would be using the path under module cache for anything
@@ -82,7 +82,7 @@ export async function getModFolderPath(fileuri: vscode.Uri): Promise<string> {
 			await promptToUpdateToolForModules('switchFormatToolToGoimports', promptMsgForFormatTool, goConfig);
 		}
 	}
-	packagePathToGoModPathMap.set(pkgPath, goModEnvResult);
+	packagePathToGoModPathMap[pkgPath] = goModEnvResult;
 	return goModEnvResult;
 }
 
