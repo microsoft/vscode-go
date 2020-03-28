e4814d0f4b127ce2d32f17f4e9ecd9531fc50e25
Zac Bergquist <zmb3@users.noreply.github.com>
2020-03-28 17:42:05 -0600

    Fix process cancelation (#3131)
    * Fix process cancelation
    
    Processes were not being killed due to the path to the external scripts
    being incorrect.  Fix this by relying on the tree-kill package rather
    than shelling out to an external script to kill a process and its
    children.
    
    Also add sinon as a dev dependency for testing purposes.
    
    Updates #3044
    
    * Use tree-kill directly in the debug adapter.
    
    We cannot take a dependency on util, which depends on the vscode module
    because the debug adapter runs in a process that doesn't have access
    to the vscode module.
    
    Co-authored-by: Ramya Achutha Rao <ramyar@microsoft.com>

--- src/util.ts
+++ src/util.ts
@@ -8,6 +8,7 @@ import fs = require('fs');
 import os = require('os');
 import path = require('path');
 import semver = require('semver');
+import kill = require('tree-kill');
 import vscode = require('vscode');
 import { NearestNeighborDict, Node } from './avlTree';
 import { buildDiagnosticCollection, lintDiagnosticCollection, vetDiagnosticCollection } from './goMain';
@@ -867,27 +868,13 @@ export function killProcess(p: cp.ChildProcess) {
 	}
 }
 
-export function killTree(processId: number): void {
-	if (process.platform === 'win32') {
-		const TASK_KILL = 'C:\\Windows\\System32\\taskkill.exe';
-
-		// when killing a process in Windows its child processes are *not* killed but become root processes.
-		// Therefore we use TASKKILL.EXE
-		try {
-			cp.execSync(`${TASK_KILL} /F /T /PID ${processId}`);
-		} catch (err) {
+export const killTree = (processId: number): void => {
+	kill(processId, (err) => {
+		if (err) {
 			console.log('Error killing process tree: ' + err);
 		}
-	} else {
-		// on linux and OS X we kill all direct and indirect child processes as well
-		try {
-			const cmd = path.join(__dirname, '../../../scripts/terminateProcess.sh');
-			cp.spawnSync(cmd, [processId.toString()]);
-		} catch (err) {
-			console.log('Error killing process tree: ' + err);
-		}
-	}
-}
+	});
+};
 
 export function makeMemoizedByteOffsetConverter(buffer: Buffer): (byteOffset: number) => number {
 	const defaultValue = new Node<number, number>(0, 0); // 0 bytes will always be 0 characters
