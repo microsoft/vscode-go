65ede8844ecce89e7edb9be360b80df64ef33019
neclepsio <neclepsio@gmail.com>
2020-04-25 07:46:04 +0200

    Handle unexpected proxy query results gracefully #3204 (#3205)

--- src/goLanguageServer.ts
+++ src/goLanguageServer.ts
@@ -455,7 +455,9 @@ async function latestGopls(tool: Tool): Promise<semver.SemVer> {
 			includePrerelease: true,
 			loose: true
 		});
-		versions.push(parsed);
+		if (parsed) {
+			versions.push(parsed);
+		}
 	}
 	if (versions.length === 0) {
 		return null;
