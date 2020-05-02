b287a0c029f2559689d43010138f84e2cb627b61
Bulent Rahim Kazanci <kazancibulent@gmail.com>
2020-05-02 21:52:23 +0300

    Prompt transform parameter input while adding tags to struct

--- src/goModifytags.ts
+++ src/goModifytags.ts
@@ -128,7 +128,14 @@ function getTagsAndOptions(config: GoTagsConfig, commandArgs: GoTagsConfig): The
 					prompt: 'Enter comma separated options'
 				})
 				.then((inputOptions) => {
-					return [inputTags, inputOptions, transformValue];
+					return vscode.window
+						.showInputBox({
+							value: transformValue,
+							prompt: 'Enter transform value'
+						})
+						.then((transformOption) => {
+							return [inputTags, inputOptions, transformOption];
+						});
 				});
 		});
 }
